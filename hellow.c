962c2d611b810adcc3e41c19caf250fc6bfa1843
Bo-Yang <byshiue@gmail.com>
2015-03-11 10:37:03 +0800

    介紹pointer

--- hellow.c
+++ hellow.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
 
-int main()
+
+void main()
 {
+
     char Name[50]="Yu-Hsun Lee"; 
+    int i = 0;
     // Name is a fixed char vecter
     // Name[0], Name[1], Name[2], ..., Name[49] 
     // Name[0]=Y
@@ -16,6 +19,17 @@ int main()
     printf( "%s\n" ,Name ) ;
     printf( "%c\n" ,Name[5] ); // Name[5] = u
     printf( "%s\n" ,Name+3); 
+    printf( "%s\n" ,Name-3); 
+    printf( "%u\n" ,Name) ; // Name -> 2686720
+    printf( "%u\n" ,Name+3) ; // Name+3 -> 2686723
+    // %u print unsigned integer  (positive integer) 
+    // %d print integer
+    for ( i ; i<50 ; i++)
+    {
+        printf( "%d:%c-%u\n", i, Name[i], Name[i]) ;
+        }
+        
+        
     system("pause") ;
      return 123 ;
     
