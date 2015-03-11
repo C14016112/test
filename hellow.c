18d9af2e3a0a7e92ab7e588b5d2cff4d942b0629
stanwang <bh329669@gmail.com>
2015-03-11 09:53:39 +0800

    用數學函數

--- hellow.c
+++ hellow.c
@@ -1,10 +1,12 @@
-#include<stdio.h>
-
+#include <stdio.h>
+#include <math.h> // cos 
 int main()
 {
     printf ( "hellow world!\n" );
    
     printf( "%d\n" ,2+3 ) ; // %d print an integer 
+    printf( "%5.5f\n" , 1.2+3.4) ; // %f printf a float
+    printf( "%.16f\n", cos(2.0*M_PI/3.0)) ; // print a cos function 
     system("pause") ;
      return 123 ;
     
