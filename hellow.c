054e3e3c8ca04f1c6f893d39dc29afa8b032b576
stanwang <bh329669@gmail.com>
2015-03-11 09:46:17 +0800

    no message

--- hellow.c
+++ hellow.c
@@ -5,7 +5,6 @@ int main()
     printf ( "hellow world!\n" );
    
     printf( "%d\n" ,2+3 ) ; // %d print an integer 
-    printf( "%5.5f\n" , 1.2+3.4) ; // %f printf a float
     system("pause") ;
      return 123 ;
     
