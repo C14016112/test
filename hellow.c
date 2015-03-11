8eb7d6178cafc8c9a251fe582432756b10aaf565
stanwang <bh329669@gmail.com>
2015-03-11 10:00:16 +0800

    print char

--- hellow.c
+++ hellow.c
@@ -2,9 +2,19 @@
 
 int main()
 {
-    printf ( "hellow world!\n" );
-   
+    char Name[50]="Yu-Hsun Lee"; 
+    // Name is a fixed char vecter
+    // Name[0], Name[1], Name[2], ..., Name[49] 
+    // Name[0]=Y
+    // Name[1]=u
+    // Name[2]=-
+    // Name[3]=H
+    // ...
+    // Name[11]=\0
+    printf( "hellow world!\n" );
     printf( "%d\n" ,2+3 ) ; // %d print an integer 
+    printf( "%s\n" ,Name ) ;
+    printf( "%c\n" ,Name[5] ); // Name[5] = u
     system("pause") ;
      return 123 ;
     
